095b2cc1f7a38562ea430515fa982e8a859d2ac4
SupawichRatchataworrakun <supawich_r@elearning.cmu.ac.th>
2021-09-30 23:35:09 +0700

    2.5

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -46,11 +46,20 @@ export default {
         description: '',
         location: '',
         organizer: { id: '', name: '' }
-      }
+      },
+      files: []
     }
   },
   methods: {
     saveEvent() {
+        Promise.all(       
+          this.files.map((file) => {
+            return EventService.uploadFile(file)
+          })       
+        ).then((response) => {
+          console.log(response)
+          console.log('finish upload file')
+        })
       EventService.saveEvent(this.event)
         .then((response) => {
           console.log(response)
@@ -69,7 +78,7 @@ export default {
         })
     },
     handleImages(files) {
-      console.log(files)
+      this.files = files
   }
 }
 }
